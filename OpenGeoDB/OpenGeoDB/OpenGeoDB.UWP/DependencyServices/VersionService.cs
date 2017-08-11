f40b718be8bb15417f37fad842e3527d516f95a6
Sebastian Kruse <sebastian@kruse-familie.eu>
2017-08-11 18:34:43 +0200

    Added VersionService to UWP.

---
+++ OpenGeoDB/OpenGeoDB/OpenGeoDB.UWP/DependencyServices/VersionService.cs
@@ -0,0 +1,16 @@
+﻿using Windows.ApplicationModel;
+using OpenGeoDB.Core.DependencyServices;
+using OpenGeoDB.UWP.DependencyServices;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(VersionService))]
+namespace OpenGeoDB.UWP.DependencyServices
+{
+    public class VersionService : IVersionService
+    {
+        public string GetAppVersion()
+        {
+            return Package.Current.Id.Version.ToString();
+        }
+    }
+}
