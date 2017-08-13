a420076f387c640d69c6977da7a02360fa3d94ae
Sebastian Kruse <sebastian@kruse-familie.eu>
2017-08-13 20:34:56 +0200

    Release Updates

--- OpenGeoDB/OpenGeoDB/OpenGeoDB.UWP/App.xaml.cs
+++ OpenGeoDB/OpenGeoDB/OpenGeoDB.UWP/App.xaml.cs
@@ -45,7 +45,7 @@ namespace OpenGeoDB.UWP
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
             {
-                this.DebugSettings.EnableFrameRateCounter = true;
+                //this.DebugSettings.EnableFrameRateCounter = true;
             }
 #endif
 
