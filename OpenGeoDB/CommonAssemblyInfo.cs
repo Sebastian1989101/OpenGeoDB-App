0a8edfafb4712c6266e3a0b1f2c9d0ee6ab02d37
Sebastian Kruse <sebastian@kruse-familie.eu>
2017-12-16 16:54:07 +0100

    Update Mapsui

--- OpenGeoDB/CommonAssemblyInfo.cs
+++ OpenGeoDB/CommonAssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright("Copyright © 2017 Sebastian Kruse")]
 [assembly: AssemblyTrademark("")]
 
-[assembly: AssemblyVersion("1.1.0.17348")]
+[assembly: AssemblyVersion("1.1.0.17350")]
 
 [assembly: InternalsVisibleTo("OpenGeoDB.UITests")] 
 [assembly: InternalsVisibleTo("OpenGeoDB.UnitTests")] 
